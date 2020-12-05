80baa466ad14b164d4b8a52544c53e298014d9f7
Sven Weih <sven.weih@gmail.com>
2020-12-05 06:53:22 +0100

    Day6 - Finding my place in the aircraft.

--- src/main/java/de/beachboys/aoc2020/Day05.java
+++ src/main/java/de/beachboys/aoc2020/Day05.java
@@ -2,16 +2,55 @@ package de.beachboys.aoc2020;
 
 import de.beachboys.Day;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Day05 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+
+        int max = 0;
+     for (String seat: input) {
+            Integer result = getSeatIDFromSeatCode(seat);
+            if (result > max) max = result;
+       }
+
+        return Integer.toString(max);
+    }
+
+    private Integer getSeatIDFromSeatCode(String seat) {
+        String row = seat.substring(0,7);
+        String col = seat.substring(7);
+
+        row = row.replace('F', '0');
+        row = row.replace('B', '1');
+
+        Integer rowI = Integer.parseInt(row, 2);
+
+        col = col.replace('R', '1');
+        col = col.replace('L','0');
+        Integer colI = Integer.parseInt(col,2);
+
+        Integer result = (rowI * 8) + colI;
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+
+        TreeSet<Integer> set = new TreeSet<>();
+        for (String seat: input) {
+            set.add(getSeatIDFromSeatCode(seat));
+        }
+
+
+        Integer lastRow =-10;
+        for (Integer i: set) {
+          if (lastRow == i-2) return Integer.toString(i-1);
+          lastRow = i;
+        }
+
+        return -100;
     }
 
 }
