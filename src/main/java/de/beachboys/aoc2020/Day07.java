a3bebec0d428de0dac28d91da55e9a718f177e01
Sven Weih <sven.weih@gmail.com>
2020-12-07 08:57:54 +0100

    Removed some code.

--- src/main/java/de/beachboys/aoc2020/Day07.java
+++ src/main/java/de/beachboys/aoc2020/Day07.java
@@ -129,23 +129,6 @@ class Bag {
         return (result+1);
     }
 
-
-
-
-    @Override
-    public boolean equals(Object obj) {
-        return ((Bag)obj).id == this.id;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-
 }
 
 class BagStore {
